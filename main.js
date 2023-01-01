3fc88240e62d2e5f445c5294f38b20ce3ff26b2d
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@icloud.com>
2023-01-01 01:35:34 +0100

    Basic countdown timer.

---
+++ main.js
@@ -0,0 +1,15 @@
+var date = new Date("January 1, 2023 00:00:00").getTime();
+
+var countdown = setInterval(function() {
+    var currentDate = new Date().getTime();
+    var delta = date - currentDate;
+    var days = Math.floor(delta / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((delta % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}, 1000);
